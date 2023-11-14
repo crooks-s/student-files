2a038cf256304ae3146400e749e1b97795681ec4
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2023-11-14 14:46:35 -0800

    fix js link, draft profiles

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 
-//Fetch 12 random users from API
+//Fetch 12 users from API
 async function getJSON(url) {
     try {
         const response = await fetch(url);
@@ -9,9 +9,23 @@ async function getJSON(url) {
     }
 }
 
+// returns an array of people objects
 async function getPeople(url) {
     const peopleJSON = await getJSON(url);
     console.log(peopleJSON);
+
+    // you have your array of objects,
+    // now iterate thru them and display: 
+    /**
+     * image -->             picture OBJECT
+     * first & last name --> name OBJECT
+     * email -->             email
+     * location -->          location OBJECT
+     */
+
+    // const profiles = peopleJSON();
+
 }
 
+// call the function to get and display 12 users
 getPeople('https://randomuser.me/api/?results=12');
