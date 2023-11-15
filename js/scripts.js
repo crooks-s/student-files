3d1b1a43830b25e29e40d37de7a61bfa1ba2dc04
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2023-11-14 17:27:43 -0800

    finish creating html for each user

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,38 @@
 
-//Fetch 12 users from API
+//Fetch 12 users from API 
 async function getJSON(url) {
     try {
         const response = await fetch(url);
-        return await response.json();
+        const data = await response.json();
+        const resultsArr = data.results;
+        return resultsArr;
+
     } catch(err) {
         throw err;
     }
 }
 
-// returns an array of people objects
+// create HTML for each person retrieved
 async function getPeople(url) {
     const peopleJSON = await getJSON(url);
-    console.log(peopleJSON);
 
-    // you have your array of objects,
-    // now iterate thru them and display: 
-    /**
-     * image -->             picture OBJECT
-     * first & last name --> name OBJECT
-     * email -->             email
-     * location -->          location OBJECT
-     */
+    for (const person of peopleJSON){
+        const gallery = document.querySelector('#gallery');
+        const div = document.createElement('div');
+        gallery.appendChild(div);
 
-    // const profiles = peopleJSON();
+        div.innerHTML = `
+        <div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="${person.picture.medium}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+            <p class="card-text">${person.email}</p>
+            <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+        </div>
+        `;
+    }
 
 }
 
