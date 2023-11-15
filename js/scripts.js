8e47cb353fba7a1ad5730fb5cd991d1775f833af
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2023-11-15 11:09:13 -0800

    add try..catch

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 const gallery = document.querySelector('#gallery');
 
 
+
 //===================
 // FETCH DATA
 //-------------------
@@ -23,6 +24,8 @@ fetchData('https://randomuser.me/api/?results=12')
     .catch(error => {
         console.error('Error: ', error);
     });
+
+
 
 //===================
 // GENERATE HTML DATA
@@ -49,25 +52,29 @@ function generateGalleryHTML(results) {
 
 // Create and populate user info for modal
 function generateModalHTML() {
-    const html = `
-        <div class="modal-container">
-        <div class="modal">
-            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-            <div class="modal-info-container">
-                <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-                <h3 id="name" class="modal-name cap">name</h3>
-                <p class="modal-text">email</p>
-                <p class="modal-text cap">city</p>
-                <hr>
-                <p class="modal-text">(555) 555-5555</p>
-                <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                <p class="modal-text">Birthday: 10/21/2015</p>
+    try {
+        const html = `
+            <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">name</h3>
+                    <p class="modal-text">email</p>
+                    <p class="modal-text cap">city</p>
+                    <hr>
+                    <p class="modal-text">(555) 555-5555</p>
+                    <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                    <p class="modal-text">Birthday: 10/21/2015</p>
+                </div>
             </div>
-        </div>
-    `;
-
-    document.body.insertAdjacentHTML('beforeend', html);
+        `;
+        document.body.insertAdjacentHTML('beforeend', html);
+    } catch (error) {
+        console.log(error.message);
+    }
 }
+
 
 
 //===================
