05cd121d1a84754a8f7ea703532ea05b2565294a
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2023-11-15 09:21:05 -0800

    close modal on Esc, draft close on close-btn

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Global DOM vars
+const gallery = document.querySelector('#gallery');
 
 //Fetch 12 users from API 
 async function getJSON(url) {
@@ -9,15 +11,14 @@ async function getJSON(url) {
 
     } catch(err) {
         throw err;
-    }
-}
+    };
+};
 
 // create HTML for each person retrieved
 async function getPeople(url) {
     const peopleJSON = await getJSON(url);
 
     for (const person of peopleJSON){
-        const gallery = document.querySelector('#gallery');
         const html = `
         <div class="card">
         <div class="card-img-container">
@@ -31,9 +32,56 @@ async function getPeople(url) {
         `;
 
         gallery.insertAdjacentHTML('beforeend', html);
-    }
+    };
 
-}
+};
+
+/**
+ * create modal window when any part of employee item is clicked
+ * include: image, name, email, city/location, cell#, detailed address, birthday
+ * 
+ * add way to close modal window
+ */
+gallery.addEventListener('click', (e) => {
+    if ( !e.target.className.includes('card') ){
+        return;
+    } else {
+        let html = `
+            <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">name</h3>
+                    <p class="modal-text">email</p>
+                    <p class="modal-text cap">city</p>
+                    <hr>
+                    <p class="modal-text">(555) 555-5555</p>
+                    <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                    <p class="modal-text">Birthday: 10/21/2015</p>
+                </div>
+            </div>
+        `;
+        document.body.insertAdjacentHTML('beforeend', html);
+    };
+});
+
+//close modal on close-btn
+// document.addEventListener('click', (e) => {
+//     const modalContainer = document.querySelector('.modal-container');
+//     const closeBtn = modalContainer.getElementsByTagName('BUTTON')[0];
+//     if (modalContainer && e.target === closeBtn){
+//         document.body.removeChild(modalContainer);
+//     }
+// });
+
+//close modal on Escape key
+document.addEventListener('keyup', (e) => {
+    const modalContainer = document.querySelector('.modal-container');
+    if (modalContainer && e.key === 'Escape') {
+        document.body.removeChild(modalContainer);
+    }
+});
 
 // call the function to get and display 12 users
 getPeople('https://randomuser.me/api/?results=12');
